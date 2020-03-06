dbaffad4c966a33eecf275bc41ecbac9e588e0fc
Philipp Pushkar <filimon43g@gmail.com>
2020-03-06 21:44:27 +0100

    [*] fix installer

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PYTHON_CONFIGURE_OPTS="--enable-shared" python3 -m venv venv &&\
 . ./venv/bin/activate &&\
 pip install -r requirements.txt &&\
 pyinstaller --onefile putpublic/__init__.py -n putpublic &&\
-cp ./dist/putpublic /usr/local/bin/ &&\
+cp -p ./dist/putpublic /usr/local/bin/ &&\
 MSG="\nSuccess!"
 rm -rf dist build *.spec
 echo -e $MSG
